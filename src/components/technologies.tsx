71da7b3d65fa1830d0282747d1e3ea1627ee28ad
hex419 <kacperinos@gmail.com>
2024-11-24 03:12:22 +0000

    ✨ add favicon and change some text

--- src/components/technologies.tsx
+++ src/components/technologies.tsx
@@ -4,7 +4,7 @@ import { Button } from "@/components/ui/button";
 export function Technologies() {
     return (
         <section className="flex min-h-0 flex-col gap-y-3">
-            <h2 className="text-lg font-bold">My Technologies</h2>
+            <h2 className="text-lg font-bold">My Most Used Technologies</h2>
             <div className="grid grid-cols-2 gap-x-2 gap-y-2 md:grid-cols-4">
                 {PORTOLFIO_DATA.technologies.map((technology) => (
                     <Button variant="outline" size="sm" key={technology.name}>
